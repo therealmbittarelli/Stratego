3857ed36bf42603ce198e1213298d82d5e7ebdec
therealmbittarelli <55117561+therealmbittarelli@users.noreply.github.com>
2019-11-29 13:05:44 -0500

    click and drag (#2)
    * adding functionality to allow user to click and drag images
    
    * correcting main.ts
    
    * making div id more descriptive
    
    * created draggable component and new instances of class

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,1 +1,8 @@
-console.log('hello world');
+import { DraggableElement } from './draggableElement';
+
+let myGandalf = new DraggableElement('mygandalf');
+myGandalf.startListening();
+let myHobbits = new DraggableElement('myhobbits');
+myHobbits.startListening();
+let myEowyn = new DraggableElement('myeowyn');
+myEowyn.startListening();
