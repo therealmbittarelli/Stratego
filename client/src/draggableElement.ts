402d979f66474e1c1bb0699bba35c3b7489786cc
therealmbittarelli <maggiekmcclellan@gmail.com>
2019-11-29 13:01:24 -0500

    created draggable component and new instances of class

---
+++ client/src/draggableElement.ts
@@ -0,0 +1,41 @@
+export class DraggableElement {
+  private elementId: string;
+  private element: HTMLElement;
+
+  private mouseOffsetX: number = 0;
+  private mouseOffsetY: number = 0;
+  private mouseStartX: number = 0;
+  private mouseStartY: number = 0;
+
+  constructor(elementId: string) {
+    this.elementId = elementId;
+    this.element = document.getElementById(this.elementId);
+  }
+
+  public startListening(): void {
+    this.element.onmousedown = this.dragMouseDown.bind(this);
+  }
+
+  private dragMouseDown(e: MouseEvent): void {
+    e.preventDefault();
+    this.mouseStartX = e.clientX;
+    this.mouseStartY = e.clientY;
+    document.onmouseup = this.closeDragElement.bind(this);
+    document.onmousemove = this.elementDrag.bind(this);
+  }
+
+  private elementDrag(e: MouseEvent): void {
+    e.preventDefault();
+    this.mouseOffsetX = this.mouseStartX - e.clientX;
+    this.mouseOffsetY = this.mouseStartY - e.clientY;
+    this.mouseStartX = e.clientX;
+    this.mouseStartY = e.clientY;
+    this.element.style.top = (this.element.offsetTop - this.mouseOffsetY) + 'px';
+    this.element.style.left = (this.element.offsetLeft - this.mouseOffsetX) + 'px';
+  }
+
+  private closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
